400d46938bf5f3447568d17b65af0b3d1595641f
waeltoumi28 <62727447+waeltoumi28@users.noreply.github.com>
2021-05-05 15:30:29 +0200

    Update EX3.py

--- EX3.py
+++ EX3.py
@@ -2,12 +2,12 @@ import pandas as pd
 fromage = pd.read_table("fromage.txt" ,sep="\t",header=0,index_col=0)
 print(fromage.shape)
 print(fromage.describe())
-from sklearn.preprocessing import StandartScaler
-sc=StandartScaler()
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
 fromage_cr=sc.fit_transform(fromage)
-from scipy.cluster.hierarchy import dendrogame , linkage
+from scipy.cluster.hierarchy import dendrogram , linkage
 import matplotlib.pyplot as plt
-Z= Linkage(fromage_cr,method='ward',metric='euclidean')
+Z= linkage(fromage_cr,method='ward',metric='euclidean')
 plt.title('C-A-H')
-dendrogam(Z,labels=fromage.index,orientation='left' , color_threshold=0)
+dendrogram(Z,labels=fromage.index,orientation='left' , color_threshold=0)
 plt.show()
